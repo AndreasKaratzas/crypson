66571ec3279855bea34255b8357d243d0cad89b0
Andreas Karatzas <andreas.karatzas@protonmail.com>
2024-03-05 21:09:36 -0600

    Minor bug fix.

--- archive/gan.py
+++ archive/gan.py
@@ -145,10 +145,10 @@ def sample_image(n_row, batches_done):
     """Saves a grid of generated digits ranging from 0 to n_classes"""
     # Sample noise
     z = Variable(torch.tensor(np.random.normal(
-        0, 1, (n_row ** 2, opt.latent_dim)), dtype=torch.float32))
+        0, 1, (n_row ** 2, opt.latent_dim)), dtype=torch.float32)).to(device)
     # Get labels ranging from 0 to n_classes for n rows
     labels = np.array([num for _ in range(n_row) for num in range(n_row)])
-    labels = Variable(torch.tensor(labels, dtype=torch.int64))
+    labels = Variable(torch.tensor(labels, dtype=torch.int64)).to(device)
     gen_imgs = generator(z, labels)
     save_image(gen_imgs.data, "images/%d.png" %
                batches_done, nrow=n_row, normalize=True)
@@ -166,10 +166,10 @@ for epoch in range(opt.n_epochs):
         batch_size = imgs.shape[0]
 
         # Adversarial ground truths
-        valid = Variable(torch.tensor(batch_size, 1, dtype=torch.float32).fill_(
+        valid = Variable(torch.tensor((batch_size, 1), dtype=torch.float32).fill_(
             1.0), requires_grad=False).to(device)
-        fake = Variable(torch.tensor(batch_size, 1, dtype=torch.float32).fill_(0.0),
-                        requires_grad=False).to(device)
+        fake = Variable(torch.tensor((batch_size, 1), dtype=torch.float32).fill_(
+            0.0), requires_grad=False).to(device)
 
         # Configure input
         real_imgs = Variable(imgs.type(torch.float32)).to(device)
