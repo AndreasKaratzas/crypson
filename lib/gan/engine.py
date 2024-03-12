50926d46bae06f093b1dde643d0e57e7db72a170
Andreas Karatzas <andreas.karatzas@protonmail.com>
2024-03-11 19:04:36 -0500

    Added debugging utility in GAN engine.

--- lib/gan/engine.py
+++ lib/gan/engine.py
@@ -197,7 +197,8 @@ class Engine(LightningModule):
             torch.arange(self.num_classes, device=self.device))
         generated_images = (generated_images + 1) / 2
         generated_images = (generated_images * 255)
-        generated_images = generated_images.transpose(2, 1, 0).squeeze().cpu().numpy().astype('uint8')
+        print(generated_images.shape)
+        generated_images = generated_images.cpu().numpy().astype('uint8')
         grid = torchvision.utils.make_grid(generated_images, nrow=5, normalize=True)        
         save_image(grid, image_path)
         self.logger.experiment.add_image("generated_images", grid, global_step=self.global_step)
