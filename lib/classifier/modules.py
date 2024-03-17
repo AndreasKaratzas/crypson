8dab3567587ec8126de19b9fe5ba9f79e0055f3f
Andreas Karatzas <andreas.karatzas@protonmail.com>
2024-03-16 22:43:47 -0500

    Added timestep embedding mechanism.

--- lib/classifier/modules.py
+++ lib/classifier/modules.py
@@ -1,36 +1,46 @@
-
 import torch.nn as nn
 
 
 class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, dropout_rate=0.2):
         super(ResidualBlock, self).__init__()
         self.fc1 = nn.Linear(in_channels, out_channels)
+        self.bn1 = nn.BatchNorm1d(out_channels)
         self.relu = nn.ReLU(inplace=True)
+        self.dropout = nn.Dropout(p=dropout_rate)
         self.fc2 = nn.Linear(out_channels, out_channels)
+        self.bn2 = nn.BatchNorm1d(out_channels)
 
         if in_channels != out_channels:
-            self.shortcut = nn.Linear(in_channels, out_channels)
+            self.shortcut = nn.Sequential(
+                nn.Linear(in_channels, out_channels),
+                nn.BatchNorm1d(out_channels)
+            )
         else:
             self.shortcut = nn.Identity()
 
     def forward(self, x):
         residual = x
         out = self.fc1(x)
+        out = self.bn1(out)
         out = self.relu(out)
+        out = self.dropout(out)
         out = self.fc2(out)
+        out = self.bn2(out)
         out += self.shortcut(residual)
         out = self.relu(out)
         return out
 
 
 class Classifier(nn.Module):
-    def __init__(self, in_dim: int, num_classes: int = 47):
+    def __init__(self, in_dim: int, num_classes: int = 47, dropout_rate=0.2):
         super(Classifier, self).__init__()
         self.features = nn.Sequential(
-            ResidualBlock(in_dim, 256),
-            ResidualBlock(256, 128),
-            ResidualBlock(128, 64),
+            ResidualBlock(in_dim, 512, dropout_rate),
+            ResidualBlock(512, 512, dropout_rate),
+            ResidualBlock(512, 256, dropout_rate),
+            ResidualBlock(256, 128, dropout_rate),
+            ResidualBlock(128, 64, dropout_rate),
             nn.Linear(64, num_classes),
         )
 
--- utils/emb.py
+++ utils/emb.py
@@ -3,58 +3,97 @@ import math
 import torch
 import torch.nn as nn
 
-from einops import repeat
+from einops import rearrange
+from einops.layers.torch import Rearrange
 
 
-def timestep_embedding(timesteps, dim, max_period=10000, repeat_only=False, device='cpu'):
-    """
-    Create sinusoidal timestep embeddings.
+def is_float_dtype(dtype):
+    return any([dtype == float_dtype for float_dtype in (torch.float64, torch.float32, torch.float16, torch.bfloat16)])
 
-    Parameters
-    ----------
-    timesteps : torch.Tensor
-        A 1-D Tensor of N indices, one per batch element. These may be fractional.
-    dim : int
-        The dimension of the output.
-    max_period : int, optional
-        Controls the minimum frequency of the embeddings.
-    repeat_only : bool, optional
-        If True, only repeat the input timesteps to the desired dimension.
-    device : str, optional
-        The device to place the output Tensor on.
-    
-    Returns
-    -------
-    torch.Tensor
-        A Tensor of shape (N, dim) of positional embeddings.
-    """
-    if not repeat_only:
-        half = dim // 2
-        freqs = torch.exp(
-            -math.log(max_period) * torch.arange(start=0,
-                                                 end=half, dtype=torch.float32) / half
-        ).to(device=device)
-        args = timesteps[:, None].float() * freqs[None]
-        embedding = torch.cat([torch.cos(args), torch.sin(args)], dim=-1)
-        if dim % 2:
-            embedding = torch.cat(
-                [embedding, torch.zeros_like(embedding[:, :1])], dim=-1)
-    else:
-        embedding = repeat(timesteps, 'b -> b d', d=dim)
-    return embedding
+
+def exists(val):
+    return val is not None
+
+
+class MLP(nn.Module):
+    def __init__(
+        self,
+        dim_in,
+        dim_out,
+        *,
+        expansion_factor=2.,
+        depth=2,
+        norm=False,
+    ):
+        super().__init__()
+        hidden_dim = int(expansion_factor * dim_out)
+        def norm_fn(): return nn.LayerNorm(hidden_dim) if norm else nn.Identity()
+
+        layers = [nn.Sequential(
+            nn.Linear(dim_in, hidden_dim),
+            nn.SiLU(),
+            norm_fn()
+        )]
+
+        for _ in range(depth - 1):
+            layers.append(nn.Sequential(
+                nn.Linear(hidden_dim, hidden_dim),
+                nn.SiLU(),
+                norm_fn()
+            ))
+
+        layers.append(nn.Linear(hidden_dim, dim_out))
+        self.net = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.net(x.float())
+
+
+class SinusoidalPosEmb(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, x):
+        dtype, device = x.dtype, x.device
+        assert is_float_dtype(
+            dtype), 'input to sinusoidal pos emb must be a float type'
+
+        half_dim = self.dim // 2
+        emb = math.log(10000) / (half_dim - 1)
+        emb = torch.exp(torch.arange(
+            half_dim, device=device, dtype=dtype) * -emb)
+        emb = rearrange(x, 'i -> i 1') * rearrange(emb, 'j -> 1 j')
+        return torch.cat((emb.sin(), emb.cos()), dim=-1).type(dtype)
 
 
 class TimeEmbedding(nn.Module):
-    def __init__(self, in_channels, time_embed_dim):
+    def __init__(self, dim,
+                 num_timesteps=None,
+                 num_time_embeds=1,
+                 device=None):
         super(TimeEmbedding, self).__init__()
-        self.fc = nn.Sequential(
-            nn.Linear(in_channels, time_embed_dim),
-            nn.SiLU(),
-            nn.Linear(time_embed_dim, time_embed_dim),
+        self.device = device
+        self.to_time_embeds = nn.Sequential(
+            nn.Embedding(num_timesteps, dim * num_time_embeds) if exists(num_timesteps) else nn.Sequential(SinusoidalPosEmb(dim),
+                                                                                                           MLP(dim, dim * num_time_embeds)),  # also offer a continuous version of timestep embeddings, with a 2 layer MLP
+            Rearrange('b (n d) -> b n d', n=num_time_embeds)
         )
 
     def forward(self, x):
-        t_emb = timestep_embedding(
-            x, self.model_channels, repeat_only=False)
-        emb = self.time_embed(t_emb)
+        emb = self.to_time_embeds(x)
         return emb
+
+
+if __name__ == "__main__":
+    # Example usage
+    b_size = 16
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    t = torch.arange(0, b_size, device=device).float()
+    print(t.shape)
+    print(t)
+    model = TimeEmbedding(dim=8, num_time_embeds=2, device=device)
+    model.to(device)
+    emb = model(t)
+    print(emb.shape)
+    print(emb)
